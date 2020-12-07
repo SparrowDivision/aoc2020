39cd301da1e7ad08c2a9531ac469375aeb7701e0
Mátyás Fehér <71931496+SparrowDivision@users.noreply.github.com>
2020-12-07 22:30:34 +0100

    2-2
    Solution for day 3

--- day3/day3.py
+++ day3/day3.py
@@ -9,7 +9,7 @@ def day1(d):
         check_point = d[x][y % len(d[x])]
         if check_point == "#": tree_count += 1
     return tree_count
-    
+
 def day2(d):
     slope = [(1,1),(3,1),(5,1),(7,1),(1,2)]
     multip = 1
@@ -19,16 +19,15 @@ def day2(d):
             x += s[1]
             y += s[0]
             check_point = d[x][y % len(d[x])]
-            if check_point == '#':
-                tree_count += 1  
+            if check_point == "#'": tree_count += 1
         multip *= tree_count
     return multip
-    
+
 def main():
-    data = readFile('day3.txt')  
+    data = readFile("day3.txt")
     print(day1(data))
     print(day2(data))
 
 if __name__ == "__main__":
     main()
-    
+
