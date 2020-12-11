b5fcad4ca8cc31813824d19e75826dc004bcd962
Mátyás Fehér <mfeher96@gmail.com>
2020-12-12 00:45:44 +0100

    1-2
    Solution for day 1

--- day01/day1.py
+++ day01/day1.py
@@ -1,20 +1,20 @@
-def readFile(input_name): 
+def readFile(input_name):
     with open(input_name, "r") as f:
         return list(map(int, f.readlines()))
-    
+
 def part1(d):
     for x in d:
         for y in d:
-            if (x + y == 2020):
-                return x*y
+            if x + y == 2020:
+                return x * y
 
-def part2(d):          
+def part2(d):
     for x in d:
         for y in d:
             for z in d:
-                if (x + y + z == 2020):
-                    return x*y*z
-    
+                if x + y + z == 2020:
+                    return x * y * z
+
 def main():
     data = readFile("day1.txt")
     print(part1(data))
